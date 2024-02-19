36e3d831c3245f7b7703f63621cc463befeaacfc
Andrii Syrokomskyi <signmotion@gmail.com>
2024-02-19 21:16:45 +0100

    Refactoring.

--- lib/src/dresser.dart
+++ lib/src/dresser.dart
@@ -3,20 +3,21 @@ part of '../replace_palette.dart';
 /// Dresses a new color palette on the image.
 class Dresser {
   /// TODO(sign): Choice the color model.
-  const Dresser();
+  const Dresser({this.distance = const RgbColorSquareDistance<int>()});
+
+  final ColorDistance distance;
 
   /// Replaces all colors in the [source] with the nearest colors
   /// from the [palette].
   Image dress(Image source, UniPalette<int> palette) {
-    const cd = RgbColorSqrtDistance<int>();
-    final memory = <int, UniColor<int>>{};
+    final cache = <int, UniColor<int>>{};
     for (final frame in source.frames) {
       for (final p in frame) {
         final color = (ColorModel.rgb, p.r.toInt(), p.g.toInt(), p.b.toInt());
-        final c = memory.update(
-          color.i255,
+        final c = cache.update(
+          color.i256,
           (v) => v,
-          ifAbsent: () => palette.closest(color, cd).$2,
+          ifAbsent: () => palette.closest(color, distance).$2,
         );
         p
           ..r = c.red
--- test/utils.dart
+++ test/utils.dart
@@ -4,15 +4,15 @@ import 'package:test/test.dart';
 //final testOutputPath = '${Directory.systemTemp.createTempSync().path}/output';
 const testOutputPath = './test/_output';
 
-void testImageEquals(Image image, Image image2) {
-  expect(image2.width, equals(image.width));
-  expect(image2.height, equals(image.height));
-  expect(image2.numChannels, equals(image.numChannels));
-  expect(image2.hasPalette, equals(image.hasPalette));
-  final c = image.iterator..moveNext();
-  for (var p2 in image2) {
-    final p1 = c.current;
-    expect(p2, equals(p1));
+void testImageEquals(Image a, Image b) {
+  expect(b.width, equals(a.width));
+  expect(b.height, equals(a.height));
+  expect(b.numChannels, equals(a.numChannels));
+  expect(b.hasPalette, equals(a.hasPalette));
+  final c = a.iterator..moveNext();
+  for (final pb in b) {
+    final pa = c.current;
+    expect(pb, equals(pa));
     c.moveNext();
   }
 }
